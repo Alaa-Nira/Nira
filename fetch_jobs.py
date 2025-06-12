953192a8046c8c9de78ca6e566f01289afdca0e6
Alaa-Nira <fiona2024a@gmail.com>
2025-06-12 12:50:22 +0300

    fetch_jobs.py

--- fetch_jobs.py
+++ fetch_jobs.py
@@ -1,30 +1,32 @@
 import requests
 import json
-from datetime import datetime
 
-# طلب الوظائف من سوريا عبر ReliefWeb
-url = "https://api.reliefweb.int/v1/jobs?appname=nira&profile=list&limit=20&filter[field]=country&filter[value]=syria"
+url = "https://api.reliefweb.int/v1/jobs?appname=nira&profile=full&limit=20&filter[field]=country&filter[value]=syria"
 
 response = requests.get(url)
 data = response.json()
 
-# استخراج الوظائف وتبسيطها إلى صيغة بسيطة
 jobs = []
 for job in data["data"]:
-    title = job["fields"]["title"]
-    org = job["fields"].get("organization", [{}])[0].get("name", "Unknown")
-    country = job["fields"].get("country", [{}])[0].get("name", "Unknown")
-    date = job.get("fields", {}).get("date", {}).get("posted", "")[:10]
-    link = job["fields"]["url"]
+    fields = job["fields"]
+
+    title = fields.get("title", "No Title")
+    org = fields.get("organization", [{}])[0].get("name", "Unknown")
+    location = fields.get("country", [{}])[0].get("name", "Unknown")
+    date = fields.get("date", {}).get("posted", "")
+    deadline = fields.get("date", {}).get("closing", "")
+    description = fields.get("description", {}).get("content", "")[:300]
+    link = fields.get("url", "#")
 
     jobs.append({
         "Job Title": title,
         "Company": org,
-        "Location": country,
-        "Date": date,
+        "Location": location,
+        "Date": date[:10],
+        "Deadline": deadline[:10],
+        "Description": description,
         "Link": link
     })
 
-# حفظها في ملف reliefweb_jobs.json
 with open("reliefweb_jobs.json", "w", encoding="utf-8") as f:
     json.dump(jobs, f, ensure_ascii=False, indent=2)
