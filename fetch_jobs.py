5c33187ea98d5414bf0f7f88bd6a20bd940f453d
Alaa-Nira <fiona2024a@gmail.com>
2025-06-12 13:10:48 +0300

    fetch_jobs.py

--- fetch_jobs.py
+++ fetch_jobs.py
@@ -1,31 +1,22 @@
 import requests
 import json
 
-url = "https://api.reliefweb.int/v1/jobs?appname=nira&profile=full&limit=20&filter[field]=country&filter[value]=syria"
-
+url = "https://api.reliefweb.int/v1/jobs?appname=nira&profile=list&limit=30&filter[field]=country&filter[value]=syria"
 response = requests.get(url)
 data = response.json()
 
 jobs = []
 for job in data["data"]:
     fields = job["fields"]
-
-    title = fields.get("title", "No Title")
-    org = fields.get("organization", [{}])[0].get("name", "Unknown")
-    location = fields.get("country", [{}])[0].get("name", "Unknown")
-    date = fields.get("date", {}).get("posted", "")
-    deadline = fields.get("date", {}).get("closing", "")
-    description = fields.get("description", {}).get("content", "")[:300]
-    link = fields.get("url", "#")
-
     jobs.append({
-        "Job Title": title,
-        "Company": org,
-        "Location": location,
-        "Date": date[:10],
-        "Deadline": deadline[:10],
-        "Description": description,
-        "Link": link
+        "Job Title": fields.get("title", "بدون عنوان"),
+        "Company": fields.get("organization", [{}])[0].get("name", "غير معروف"),
+        "City": fields.get("city", ["غير محددة"])[0],
+        "Description": fields.get("description", {}).get("content", "").replace("\n", " ")[:200],
+        "Tags": [tag.get("name", "") for tag in fields.get("theme", [])],
+        "Category": fields.get("career_categories", [{}])[0].get("name", ""),
+        "Type": fields.get("job_type", "غير محدد"),
+        "Link": fields.get("url", "#")
     })
 
 with open("reliefweb_jobs.json", "w", encoding="utf-8") as f:
